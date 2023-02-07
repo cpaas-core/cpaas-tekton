bb3744e45d23e198df10b1b9b009418330b9e88a
Oleg Betsun <obetsun@redhat.com>
2023-02-07 12:23:20 +0200

    obetsun 02-clustertasks

--- obetsun/02-clustertasks/run-pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tkn pipeline start cluster-task-pipeline \
-    -n default \
-    --showlog \
-    --workspace name=from-beyond,volumeClaimTemplateFile=pvc.yaml \
-    -p message="messsuge" --use-param-defaults
